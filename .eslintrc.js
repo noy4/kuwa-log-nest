fffaf4dd7e4da1593dc6430b8c1afc519ea30daf
noy4 <naoyan418@gmail.com>
2021-08-09 18:40:39 +0900

    api叩けるとこまで

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-hooks', 'react', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'jsx-quotes': ['error', 'prefer-single'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    // semi: ['error', 'never'],
+    // quotes: ['error', 'single'],
+  },
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import 'tailwindcss/tailwind.css'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
+}
+export default MyApp
